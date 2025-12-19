60ea639f539d7a532437c436572f380507e1bde4
PaarthGilhotra13 <paarthgilhotra.544@gmail.com>
2025-12-19 21:14:48 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
 import job from "./config/cron.js";
+import ENV from "./config/env.js";
 
 if (ENV.NODE_ENV ==="production")job.start()
 const app = express()
